425f6461ad8919de74dccc8b2a643ed4a8cea32d
Jack Enqvist <jack.enqvist@gmail.com>
2020-06-25 00:06:37 +0200

    Initial Commit

---
+++ StoryboardApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StoryboardApp
+//
+//  Created by Jack Enqvist on 2020-06-25.
+//  Copyright © 2020 Jack Enqvist. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StoryboardApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StoryboardApp
+//
+//  Created by Jack Enqvist on 2020-06-25.
+//  Copyright © 2020 Jack Enqvist. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
